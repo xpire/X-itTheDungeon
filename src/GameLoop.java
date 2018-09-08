f8e91495d1b8f19ccb332e10c6fe46020af990dd
Ian Park <ian.isu.park@gmail.com>
2018-09-08 17:28:29 +1000

    Set up the game loop

---
+++ src/GameLoop.java
@@ -0,0 +1,91 @@
+import javafx.animation.AnimationTimer;
+
+import java.util.function.Consumer;
+
+public class GameLoop extends AnimationTimer {
+
+    private final float NANO_PER_SEC = 1000000000f;
+    private final float SEC_PER_TICK = 1 / 60f;
+    private final float MAX_TICKS_PER_FRAME = 5;
+    private final int LAG_CAP = 100; // 100 seconds
+
+    private long prevTime = 0;
+    private float lag = 0;
+
+    private float secondsElapsed = 0.0f;
+    private int nFrames = 0;
+
+    private Game game;
+    private Consumer<Integer> fpsReporter;
+
+
+//    private Runnable preUpdater;
+//    private Consumer<Float> updater;
+//    private Runnable postUpdater;
+
+
+//    public GameLoop(Runnable preUpdater, Consumer<Float> updater, Runnable postUpdater, Consumer<Integer> fpsReporter) {
+//        this.preUpdater     = preUpdater;
+//        this.updater        = updater;
+//        this.postUpdater    = postUpdater;
+//        this.fpsReporter    = fpsReporter;
+//    }
+
+    public GameLoop(Game game, Consumer<Integer> fpsReporter) {
+        this.game = game;
+        this.fpsReporter = fpsReporter;
+    }
+
+
+    @Override
+    public void handle(long currTime)
+    {
+        if (prevTime == 0) {
+            prevTime = currTime;
+            return;
+        }
+
+        lag += (currTime - prevTime) / NANO_PER_SEC;
+
+        // Input Processing
+        game.onBeforeUpdate();
+
+        // Update Loop
+        int count = 0;
+        while (lag >= SEC_PER_TICK && count < MAX_TICKS_PER_FRAME) {
+            game.onUpdate(SEC_PER_TICK);
+            lag -= SEC_PER_TICK;
+            count++;
+        }
+
+        // Rendering
+        game.onAfterUpdate();
+
+        // Float overflow handling
+        if (lag > LAG_CAP) {
+            lag = LAG_CAP / 10.0f;
+        }
+
+        // FPS reporting
+        nFrames++;
+        secondsElapsed += (currTime - prevTime) / NANO_PER_SEC;
+        if (secondsElapsed >= 2.0f) {
+            fpsReporter.accept(Math.round(nFrames / secondsElapsed));
+            nFrames = 0;
+            secondsElapsed = 0.0f;
+        }
+
+        prevTime = currTime;
+    }
+
+
+    @Override
+    public void stop()
+    {
+        super.stop();
+        prevTime = 0;
+        lag = 0.0f;
+        nFrames = 0;
+        secondsElapsed = 0.0f;
+    }
+}
---
+++ src/Input.java
@@ -0,0 +1,56 @@
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.util.HashMap;
+
+public class Input implements EventHandler<KeyEvent> {
+
+    private HashMap<KeyCode, Boolean> prevKeys = new HashMap<>();
+    private HashMap<KeyCode, Boolean> keys = new HashMap<>();
+
+
+    public void onKeyEvent(KeyEvent evt) {
+
+        KeyCode code = evt.getCode();
+
+        if (evt.getEventType() == KeyEvent.KEY_PRESSED) {
+            keys.put(code, true);
+
+        } else if (evt.getEventType() == KeyEvent.KEY_RELEASED) {
+            keys.put(evt.getCode(), false);
+        }
+    }
+
+
+    public void update() {
+        prevKeys = new HashMap<>(keys);
+    }
+
+    public void processInputs() {
+
+    }
+
+
+    public Boolean isDown(KeyCode code) {
+        return !wasHeld(code) && isHeld(code);
+    }
+
+    public Boolean isUp(KeyCode code) {
+        return wasHeld(code) && !isHeld(code);
+    }
+
+
+    public Boolean isHeld(KeyCode code) {
+        return keys.getOrDefault(code, false);
+    }
+
+    private Boolean wasHeld(KeyCode code) {
+        return prevKeys.getOrDefault(code, false);
+    }
+
+    @Override
+    public void handle(KeyEvent event) {
+        onKeyEvent(event);
+    }
+}
