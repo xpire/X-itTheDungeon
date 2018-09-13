2437cf82a42107ada69149dc5ed8a86773b98d28
Song <z5165313@student.unsw.edu.au>
2018-09-14 01:10:49 +1000

    beginning of create mode; commit so i can checkout

---
+++ src/main/maploading/Draft.java
@@ -0,0 +1,14 @@
+package main.maploading;
+
+public class Draft {
+
+    public void CreateDraft() {
+        TileMap tileMap = new TileMap(8, 8);
+
+    }
+
+    public static void main(String[] args) {
+        Draft d = new Draft();
+        d.CreateDraft();
+    }
+}
