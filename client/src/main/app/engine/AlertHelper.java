8529b704ecd47fb340d7b11000a5f66807b5dd91
Song <z5165313@student.unsw.edu.au>
2018-10-21 20:50:49 +1100

    app.engine javadoc done

--- client/src/main/app/engine/AlertHelper.java
+++ client/src/main/app/engine/AlertHelper.java
@@ -1,11 +1,18 @@
 package main.app.engine;
 
-import javafx.application.Application;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import main.app.model.PlayModeSelectScreen;
 
+/**
+ * Class which prints out alerts to the application
+ */
 public class AlertHelper {
+
+    /**
+     * Displays an alert to the screen
+     * @param alertType type of alert
+     * @param title : title of the alert
+     * @param message : alert message
+     */
     public static void showAlert(Alert.AlertType alertType, String title, String message) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
--- client/src/main/app/engine/Game.java
+++ client/src/main/app/engine/Game.java
@@ -1,16 +1,33 @@
 package main.app.engine;
 
-
+/**
+ * Interface of the main game
+ */
 public interface Game {
 
+    /**
+     * Actions to occur when the game begins
+     */
     void onStart();
 
+    /**
+     * Actions to occur during the beginning of a tick
+     */
     void onUpdateBegin();
 
+    /**
+     * Actions to occur during the game tick
+     */
     void onUpdate();
 
+    /**
+     * Actions to occur at the end of a game tick
+     */
     void onUpdateEnd();
 
+    /**
+     * Actions to occur when the game ends
+     */
     void onStop();
 }
 
